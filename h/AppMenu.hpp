254639867865fe82e6d1acb9a7b46439000c8a29
Niko <no001here@gmail.com>
2025-11-16 16:45:42 +0100

    LSM Tree implementation with ui for testing

---
+++ h/AppMenu.hpp
@@ -0,0 +1,16 @@
+	#ifndef APPMENU_HPP
+	#define APPMENU_HPP
+	
+	#include <string>
+	
+	class AppMenu {
+	public:
+		void run();
+	private:
+		void printMenu();
+		void lsmOperations();
+	};
+	
+	#endif // APPMENU_HPP
+	
+
